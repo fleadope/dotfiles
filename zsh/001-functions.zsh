b48b74cef77b5de68d0e081c7fa310eab7c75a77
Lee Pope <leepope@gmail.com>
2013-08-17 12:43:03 -0700

    This may become useful

--- zsh/001-functions.zsh
+++ zsh/001-functions.zsh
@@ -16,3 +16,28 @@ isutfenv() {
         *)     return 1 ;;
     esac
 }
+# h/t [http://mivok.net/2009/09/20/bashfunctionoverrist.html]
+
+save_function()
+{     local ORIG_FUNC=$(declare -f )
+      local NEWNAME_FUNC="${ORIG_FUNC#}"   
+      eval "$NEWNAME_FUNC"
+}
+
+# like source ~/.mybash/_functions
+# then do your chruby setup..
+
+#chruby_setup_goes_here...
+
+## then:
+
+#save_function chruby old_chruby
+#source chruby_override_w_after_use_hook
+
+## like: source ~/.mybash/_chruby_override
+## the new function looks like:
+
+#function chruby () {                                                                                                           
+    #old_chruby $*                                                                                                              
+    #your_after_use_hook_code_here..                              
+#}
