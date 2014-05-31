77b0671fad60ae31dc776a7a371442fcde3876d5
fleadope <leepope@gmail.com>
2014-05-31 15:05:12 -0700

    got in a hurry, part of removing this mechanism of checking for updates.
    run 'script/install-cron' on new machines.

--- tools/upgrade.sh
+++
@@ -1,9 +0,0 @@
-printf '\033[0m\n' "Upgrading Dotfiles"
-cd "$DOTFILES_DIR"
-if git pull --rebase --stat origin master
-then
-    printf '\033[0;34m%s\033[0m\n' 'Hooray! Dotfiles have been updated and/or are at the current version.'
-else
-    printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
-fi
-
