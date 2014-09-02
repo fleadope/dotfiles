bfbb833d971c7a5d1266112be179e8693b7e4e1f
fleadope <leepope@gmail.com>
2014-09-02 15:54:47 -0700

    moved from a zsh function to a bash script

--- zsh/001-functions.zsh
+++ zsh/001-functions.zsh
@@ -29,37 +29,3 @@ command_exists() {
   return $?
 }
 
-update_default_gems() {
-    if [ -f "${RBENV_ROOT}/default-gems" ]; then
-    local line gem_name gem_version args
-
-    # Read gem names and versions from $RBENV_ROOT/default-gems.
-    while IFS=" " read -r -a line; do
-
-      # Skip empty lines.
-      [ "${#line[@]}" -gt 0 ] || continue
-
-      # Skip comment lines that begin with `#`.
-      [ "${line[0]:0:1}" != "#" ] || continue
-
-      gem_name="${line[0]}"
-      gem_version="${line[1]}"
-
-      if [ "$gem_version" == "--pre" ]; then
-        args=( --pre )
-      elif [ -n "$gem_version" ]; then
-        args=( --version "$gem_version" )
-      else
-        args=()
-      fi
-
-      # Invoke `gem install` in the just-installed Ruby. Point its
-      # stdin to /dev/null or else it'll read from our default-gems
-      # file.
-      RBENV_VERSION="$VERSION_NAME" rbenv-exec gem install "$gem_name" "${args[@]}" < /dev/null || {
-        echo "rbenv: error installing gem \`$gem_name'"
-      } >&2
-
-    done < "${RBENV_ROOT}/default-gems"
-  fi
-}
