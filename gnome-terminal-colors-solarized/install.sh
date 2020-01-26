83e162819a5502ba731b44e69449d6e9f090761d
Lee Pope <leepope@gmail.com>
2020-01-25 22:40:49 -0800

    didn't really like solarized in the terminal

--- gnome-terminal-colors-solarized/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install fonts-powerline dconf-cli
-
-cd $(dirname $0)
-
-git clone https://github.com/aruhier/gnome-terminal-colors-solarized.git
-cd gnome-terminal-colors-solarized
-./install.sh
